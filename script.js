5aecec64da4a33f91095ec8170881b48e4d2e954
Aboubakr Elbouzidi <aboubakerelbouzidi@gmail.com>
2023-03-07 22:36:52 +0100

    add arrays to the classes and display the whole page from the Script

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ episodeTitle = episode[propertyToAccess];// "The red city"
 
 */
 
+/*
 // Create a class here
 // =====================================
 
@@ -58,7 +59,7 @@ class Episode {
   
   let secondEpisode = new Episode("The new offer", 45, true)
   
-  let thirdEpisode = new Episode("Ghost Soldiers", 60, false)
+  let thirdEpisode = new Episode("The Unexpected Climax", 60, false)
   // =====================================
   
   document.querySelector('#first-episode-info').innerText = `Episode: ${firstEpisode.title}
@@ -72,3 +73,40 @@ class Episode {
   document.querySelector('#third-episode-info').innerText = `Episode: ${thirdEpisode.title}
   Duration: ${thirdEpisode.duration} min
   ${thirdEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+  */
+
+  class Episode {
+    constructor(title, duration, hasBeenWatched) {
+      this.title = title;
+      this.duration = duration;
+      this.hasBeenWatched = hasBeenWatched;
+    }
+  }
+  
+  let firstEpisode = new Episode('Dark Beginnings', 45, true);
+  let secondEpisode = new Episode('The Mystery Continues', 45, false);
+  let thirdEpisode = new Episode('An Unexpected Climax', 60, false);
+  
+  // Create your array here
+  // ====================================
+  
+  let episodes = [firstEpisode, secondEpisode, thirdEpisode]
+  
+  
+  // ====================================
+  
+  const body = document.querySelector('body');
+  
+  for(let i = 0; i < 12; i++) {
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('series-frame');
+    let newTitle = document.createElement('h2');
+    newTitle.innerText = 'The Story of Tau';
+    let newParagraph = document.createElement('p');
+    newParagraph.innerText = `${episodes[i].title}
+  ${episodes[i].duration} minutes
+  ${episodes[i].hasBeenWatched ? 'Already been watched' : 'Not yet watched'}`;
+    newDiv.append(newTitle);
+    newDiv.append(newParagraph);
+    body.append(newDiv);
+  }
