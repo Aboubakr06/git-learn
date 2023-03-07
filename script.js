427ceb2051c8430f5762b70d8188987bb3e2d281
Aboubakr Elbouzidi <aboubakerelbouzidi@gmail.com>
2023-03-07 22:22:21 +0100

    add JS Classes and modify html and css

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ Duration: ${episodeDuration} min
 ${hasBeenWatched ? 'Already watched' : 'Not yet watched'}`
 */
 
+/*
 // Create Object here
 // =========================================
 
@@ -30,3 +31,44 @@ let episode = {
   document.querySelector('#episode-info').innerText = `Episode: ${episode.title}
   Duration: ${episode.duration} min
   ${episode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`
+  
+let episodeTitle = episode.title; 
+let episodeDuration = episode.duration  
+
+episodeTitle = episode["title"]; // "L'Histoire de Tao"
+episodeDuration = episode["duration"]; // 250
+
+let propertyToAccess = "title";
+episodeTitle = episode[propertyToAccess];// "The red city"
+
+*/
+
+// Create a class here
+// =====================================
+
+class Episode {
+    constructor(title, duration, hasBeenWatched) {
+      this.title = title,
+      this.duration = duration,
+      this.hasBeenWatched = hasBeenWatched
+    }
+  }
+  
+  let firstEpisode = new Episode("The red city", 60, true)
+  
+  let secondEpisode = new Episode("The new offer", 45, true)
+  
+  let thirdEpisode = new Episode("Ghost Soldiers", 60, false)
+  // =====================================
+  
+  document.querySelector('#first-episode-info').innerText = `Episode: ${firstEpisode.title}
+  Duration: ${firstEpisode.duration} min
+  ${firstEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+  
+  document.querySelector('#second-episode-info').innerText = `Episode: ${secondEpisode.title}
+  Duration: ${secondEpisode.duration} min
+  ${secondEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
+  
+  document.querySelector('#third-episode-info').innerText = `Episode: ${thirdEpisode.title}
+  Duration: ${thirdEpisode.duration} min
+  ${thirdEpisode.hasBeenWatched ? 'Already watched' : 'Not yet watched'}`;
