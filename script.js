81bbd5f51f504aa69c4ce7efab8dd0e0597d8067
Aboubakr Elbouzidi <aboubakerelbouzidi@gmail.com>
2023-03-08 23:35:58 +0100

    add a loop to script.js change hasBeenWatched to false

--- script.js
+++ script.js
@@ -116,6 +116,8 @@ class Episode {
 
   */
 
+ /*
+
   class Episode {
     constructor(title, duration, hasBeenWatched) {
       this.title = title;
@@ -142,6 +144,56 @@ class Episode {
   
   const body = document.querySelector('body');
   
+  document.querySelector('#episodes').innerText = numberOfEpisodes;
+  
+  for(let episode of episodes) {
+    let newDiv = document.createElement('div');
+    newDiv.classList.add('series-frame');
+    let newTitle = document.createElement('h2');
+    newTitle.innerText = 'The Story of Tau';
+    let newParagraph = document.createElement('p');
+    newParagraph.innerText = `${episode.title}
+  ${episode.duration} minutes
+  ${episode.hasBeenWatched ? 'Already been watched' : 'Not yet watched'}`;
+    newDiv.append(newTitle);
+    newDiv.append(newParagraph);
+    body.append(newDiv);
+  }
+
+  */
+
+  class Episode {
+    constructor(title, duration, hasBeenWatched) {
+      this.title = title;
+      this.duration = duration;
+      this.hasBeenWatched = hasBeenWatched;
+    }
+  }
+  
+  let episodes = [
+    new Episode('Dark Beginnings', 45, true),
+    new Episode('The Mystery Continues', 45, false),
+    new Episode('An Unexpected Climax', 60, false)
+  ];
+
+  let numberOfEpisodes = episodes.length;
+  
+  // Add logic here
+  // ======================
+  
+  for (let i = 0 ; i < episodes.length ; i++) {
+      episodes[i].hasBeenWatched = false;
+  }
+  /*
+  
+  for (let episode of episodes) {
+  episode.hasBeenWatched = false;
+}
+  */     
+  // ======================
+  
+  const body = document.querySelector('body');
+
   document.querySelector('#episodes').innerText = numberOfEpisodes;
   
   for(let episode of episodes) {
