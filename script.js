3ee31c2d79e1fe5e60134ea2b9453c5cc7c7c156
Aboubakr Elbouzidi <aboubakerelbouzidi@gmail.com>
2023-03-11 20:58:34 +0100

    refactor a repeated code and use a function in script.js

--- script.js
+++ script.js
@@ -258,3 +258,69 @@ if (tauAverage && colinAverage) {
 }
 
 */
+
+/* ======================= add 
+
+class Show {
+    constructor(title, numberOfSeasons, episodesPerSeason) {
+      this.title = title;
+      this.numberOfSeasons = numberOfSeasons;
+      this.episodesPerSeason = episodesPerSeason;
+  }
+}
+
+const tau = new Show('The Story of Tau', 5, 12);
+const meldrum = new Show('The Hero of Old Meldrum', 3, 6);
+const clara = new Show('The Bugs of Isla Clara', 6, 15);
+const narcos = new Show('Narcos', 3, 10);
+
+const shows = [tau, meldrum, clara, narcos];
+
+// Modify the following code
+// ======================
+
+const showComponent = (show) => {
+  const titleText = show.title;
+const seasonsText = show.numberOfSeasons + ' seasons';
+const episodesText = show.episodesPerSeason + ' episodes per season';
+return {
+  titleText,
+  seasonsText,
+  episodesText
+};
+}
+
+const tauComponent = showComponent(tau)
+const meldrumComponent = showComponent(meldrum)
+const claraComponent = showComponent(clara)
+const narcosComponent = showComponent(narcos)
+
+const showComponents = [tauComponent, meldrumComponent, claraComponent, narcosComponent];
+
+// ======================
+// Modify the code above
+
+const body = document.querySelector('body');
+
+const updateShows = () => {
+  for (let show of showComponents) {
+    const showPane = document.createElement('div');
+    showPane.classList.add('series-frame');
+    const showHeading = document.createElement('h2');
+    showHeading.innerText = show.titleText;
+    const showDetails = document.createElement('p');
+    const seasons = document.createElement('p');
+    seasons.innerText = show.seasonsText;
+    const episodes = document.createElement('p');
+    episodes.innerText = show.episodesText;
+    showDetails.append(seasons);
+    showDetails.append(episodes);
+    showPane.append(showHeading);
+    showPane.append(showDetails);
+    body.append(showPane);
+  }
+};
+
+updateShows();
+
+*/
