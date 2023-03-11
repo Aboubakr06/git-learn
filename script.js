7a278e3c10a120c275b684898a11080b219f6ba8
Aboubakr Elbouzidi <aboubakerelbouzidi@gmail.com>
2023-03-11 21:36:40 +0100

    add word counting script

--- script.js
+++ script.js
@@ -324,3 +324,37 @@ const updateShows = () => {
 updateShows();
 
 */
+
+
+/* count word 
+
+const getWordCount = (stringToTest) => {
+    const wordArray = stringToTest.split(" ");
+    return wordArray.length;
+}
+    
+const getLetterCount = (stringToTest) => {
+    const wordArray = stringToTest.split(" ");
+    let totalLetters = 0;
+    
+    for (let word of wordArray) {
+        word = word.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+        totalLetters += word.length;
+    }
+    
+    return totalLetters;
+}
+    
+const getAverageWordLength = (stringToTest) => {
+    return parseFloat((getLetterCount(stringToTest) / getWordCount(stringToTest)).toFixed(2));
+}
+    
+const printStringStats = (stringToTest) => {
+    console.log({
+        wordCount: getWordCount(stringToTest),
+        letterCount: getLetterCount(stringToTest),
+        averageWordLength: getAverageWordLength(stringToTest)
+    })
+}
+
+*/
